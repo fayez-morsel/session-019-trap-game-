45cbda5a8431bf76833d239b9817ad51c85dfffd
Fayez Morsel <fayezmorsel77@gmail.com>
2025-08-29 21:45:17 +0300

    audio

--- app.js
+++ app.js
@@ -15,3 +15,33 @@ let playerPos = { r: -1, c: -1 };
 let boardData = [];
 let flashTimeoutId = null;
 let muted = false;
+
+// ---------------- AUDIO ----------------
+let audioCtx;
+
+function beep(freq, dur = 100, type = "sine") {
+  if (muted) return;
+  if (!audioCtx) {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  }
+  if (audioCtx.state === "suspended") {
+    audioCtx.resume();
+  }
+
+  let osc = audioCtx.createOscillator();
+  let gain = audioCtx.createGain();
+
+  osc.type = type;
+  osc.frequency.value = freq;
+  gain.gain.setValueAtTime(0.5, audioCtx.currentTime);
+  gain.gain.exponentialRampToValueAtTime(
+    0.001,
+    audioCtx.currentTime + dur / 1000
+  );
+
+  osc.connect(gain);
+  gain.connect(audioCtx.destination);
+
+  osc.start(audioCtx.currentTime);
+  osc.stop(audioCtx.currentTime + dur / 1000);
+}
